778cffea6321238c7edba024f6cce3cb0dc47788
Sumin Byeon <suminb@gmail.com>
2011-02-05 17:28:56 -0700

    Webthumb basic functionalities.

--- webthumb/views.py
+++ webthumb/views.py
@@ -1,2 +1,13 @@
+from django.http import HttpResponse
 from google.appengine.api import images
+from urllib2 import urlopen
 
+def resize(request):
+    url = request.GET['url']
+    f = urlopen(url)
+    image = f.read()
+    f.close()
+    
+    thumbnail = images.resize(image, 512, 512)
+    
+    return HttpResponse(thumbnail, mimetype="image/jpeg")
