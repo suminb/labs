d4bdf7563496cd2606bf00ff1a6f0294bd26751f
Sumin Byeon <suminb@gmail.com>
2011-07-27 02:00:54 +0900

    Removed unused code.

--- hanja/urls.py
+++ hanja/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'hanja.views.index'),
-    (r'^lookup$', 'hanja.views.lookup'),
 )
--- hanja/views.py
+++ hanja/views.py
@@ -1,22 +1,4 @@
- # -*- coding: utf-8
-
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render_to_response
-
-from lib.frontend import *
-from hanja.dict import *
+from django.http import HttpResponseRedirect
 
 def index(request):
     return HttpResponseRedirect('http://hanja.suminb.com')
-#    r = request.REQUEST
-#    return render_to_response('hanja/index.html',
-#        {'query': r['q']} if 'q' in r else '')
-
-def lookup(request):
-    if 'q' in request.REQUEST:
-        query = request.REQUEST['q']
-        result = ''.join(map(lambda x: table[x] if x in table else x, query))
-
-        return response_ok(result)
-    else:
-        return response_error('Invalid request')
