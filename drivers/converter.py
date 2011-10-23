605d13a0c82e51d61292817e515aca8d621d580b
Sumin Byeon <suminb@gmail.com>
2011-10-23 01:50:49 -0700

    More refinements.

--- drivers/converter.py
+++ drivers/converter.py
@@ -6,8 +6,24 @@ contains data in a JSON format, so our frontend can display it.
 
 import csv
 import json
+import operator
 
 
+def aggregate_as_pairs(rows, key):
+    """
+    Returns sorted list of key/value pairs, by value, in descending order.
+    
+    @param rows List containing raw data extracted from CSV
+    @param key Field name to aggregate by (e.g. 'Year', 'Make', etc.)
+    """
+    dict = {}
+    for row in rows:
+        k = row[key] if row[key].strip() != '' else 'Unknown'
+        dict[k] = dict[k] + 1 if k in dict else 1
+        
+    # Sort by value
+    return sorted(dict.iteritems(), key=operator.itemgetter(1), reverse=True)
+    
 def write_kv_pairs(fout, varname, pairs):
     """
     @param fout File pointer
@@ -15,30 +31,18 @@ def write_kv_pairs(fout, varname, pairs):
     @param pairs Key/value pairs to be written
     """
     fout.write('var %s = [' % varname);
-    fout.write( ',\n    '.join( map(lambda k: "['%s', %d]" % (k, pairs[k]), pairs) ) )
+    fout.write( ',\n    '.join( map(lambda p: "['%s', %d]" % (p[0], p[1]), pairs) ) )
     fout.write('];\n\n')
-
 
 rows = csv.DictReader(open('data.csv', 'rb'), delimiter=',', quotechar='|')
 
 slow_drivers = filter(lambda r: r['Driver Type'] == 'Slow', rows)
 
-#map(lambda d: [d['Make'],  slow_drivers
-
-slow_by_make = {}
-for sd in slow_drivers:
-    make = sd['Make']
-    slow_by_make[make] = slow_by_make[make] + 1 if make in slow_by_make else 1
-    
-slow_by_sex = {}
-for sd in slow_drivers:
-    key = sd['Driver Sex']
-    if key.strip() == '': key = 'Unknown'
-    slow_by_sex[key] = slow_by_sex[key] + 1 if key in slow_by_sex else 1
-
 fout = open('data.js', 'w')
 
-write_kv_pairs(fout, 'byMake', slow_by_make)
-write_kv_pairs(fout, 'bySex', slow_by_sex)
+write_kv_pairs(fout, 'byMake', aggregate_as_pairs(slow_drivers, 'Make'))
+write_kv_pairs(fout, 'byBodyType', aggregate_as_pairs(slow_drivers, 'Body Type'))
+write_kv_pairs(fout, 'byColor', aggregate_as_pairs(slow_drivers, 'Color'))
+write_kv_pairs(fout, 'bySex', aggregate_as_pairs(slow_drivers, 'Driver Sex'))
 
 fout.close()
