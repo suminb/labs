68f0df0d31a943cab86478c8ad25f04c660077f1
Sumin Byeon <suminb@gmail.com>
2011-10-23 01:36:34 -0700

    Slow drivers by sex.

--- drivers/converter.py
+++ drivers/converter.py
@@ -7,6 +7,18 @@ contains data in a JSON format, so our frontend can display it.
 import csv
 import json
 
+
+def write_kv_pairs(fout, varname, pairs):
+    """
+    @param fout File pointer
+    @param varname JavaScript variable name
+    @param pairs Key/value pairs to be written
+    """
+    fout.write('var %s = [' % varname);
+    fout.write( ',\n    '.join( map(lambda k: "['%s', %d]" % (k, pairs[k]), pairs) ) )
+    fout.write('];\n\n')
+
+
 rows = csv.DictReader(open('data.csv', 'rb'), delimiter=',', quotechar='|')
 
 slow_drivers = filter(lambda r: r['Driver Type'] == 'Slow', rows)
@@ -17,10 +29,16 @@ slow_by_make = {}
 for sd in slow_drivers:
     make = sd['Make']
     slow_by_make[make] = slow_by_make[make] + 1 if make in slow_by_make else 1
-
+    
+slow_by_sex = {}
+for sd in slow_drivers:
+    key = sd['Driver Sex']
+    if key.strip() == '': key = 'Unknown'
+    slow_by_sex[key] = slow_by_sex[key] + 1 if key in slow_by_sex else 1
 
 fout = open('data.js', 'w')
-fout.write('var _data = [');
-fout.write( ','.join( map(lambda d: "['%s', %d]\n" % (d, slow_by_make[d]), slow_by_make) ) )
-fout.write('];')
-fout.close();
+
+write_kv_pairs(fout, 'byMake', slow_by_make)
+write_kv_pairs(fout, 'bySex', slow_by_sex)
+
+fout.close()
