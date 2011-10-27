706785826e4f609fd5b93a697a08e363d7fde994
Sumin Byeon <suminb@gmail.com>
2011-10-27 10:19:15 -0700

    Displaying datetime that data was lastly updated.

--- drivers/views.py
+++ drivers/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render_to_response
+from datetime import datetime
+import os
 
 def index(request):
-    return render_to_response('drivers/index.html', {});
+    filestat = os.stat('drivers/data.js')
+    data_last_updated = datetime.utcfromtimestamp(filestat.st_mtime)
+    return render_to_response('drivers/index.html', {'data_last_updated': data_last_updated.strftime("%Y-%m-%d %H:%M:%S")});
